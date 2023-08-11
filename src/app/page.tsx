b06456478784fe615e65becb0d1c6cfeda1d4ce8
Samuel Silva <samuel7silva@outlook.com>
2023-08-11 19:25:50 -0300

    fix: link style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between bg-white p-24 dark:bg-black">
-      <Link href="/signin" className="rounded bg-black px-6 py-2 text-white">
+      <Link
+        href="/signin"
+        className="rounded bg-black px-6 py-2 text-white dark:bg-white dark:text-black"
+      >
         Login
       </Link>
     </div>
