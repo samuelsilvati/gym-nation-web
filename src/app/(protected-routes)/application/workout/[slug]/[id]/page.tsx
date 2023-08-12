dcdabe0a2b89812cab96536dc2961a7515c8919b
Samuel Silva <samuel7silva@outlook.com>
2023-08-12 09:27:09 -0300

    add dialogs

--- src/app/(protected-routes)/application/workout/[slug]/[id]/page.tsx
+++ src/app/(protected-routes)/application/workout/[slug]/[id]/page.tsx
@@ -3,11 +3,33 @@ import DayOfWeek from '@/components/dayOfweek'
 import { Card } from '@/components/ui/card'
 import { api } from '@/lib/api'
 
-import { ChevronLeft, Loader2, Trash2 } from 'lucide-react'
+import { ChevronLeft, Loader2, Trash2, X } from 'lucide-react'
 import { useSession as UseSession } from 'next-auth/react'
 import UseSWR from 'swr'
 
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 interface ExerciseProps {
   id: number
@@ -21,7 +43,6 @@ interface ExerciseProps {
 
 function page({ params }: { params: { slug: string; id: string } }) {
   const { data: session } = UseSession()
-
   const fetcher = (url: string) =>
     api
       .get(url, {
@@ -95,14 +116,97 @@ function page({ params }: { params: { slug: string; id: string } }) {
                   </div>
                 </div>
               </button>
-              <div className="flex w-16 items-center justify-center">
-                <button type="button" className="flex items-center p-2">
-                  <Trash2 />
-                </button>
-              </div>
+              <form className="flex justify-center">
+                <Dialog>
+                  <DialogTrigger className="text-red-900 dark:text-red-200">
+                    <Trash2 />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                      <DialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your account and remove your data from our
+                        servers.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                      <Button type="submit" variant={'destructive'}>
+                        Remove
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </form>
             </Card>
           ))}
         </div>
+        <form className="mx-auto pt-4 text-center">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Novo exercício</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Novo exercício</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you{"'"}re
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4 bg-slate-400">
+                  <Select onValueChange={(value) => console.log(value)}>
+                    <SelectTrigger className="col-span-4">
+                      <SelectValue placeholder="Selecione um grupo." />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Grupos</SelectLabel>
+                        <SelectItem value="1">Peito</SelectItem>
+                        <SelectItem value="2">Costas</SelectItem>
+                        <SelectItem value="3">Pernas</SelectItem>
+                        <SelectItem value="4">Ombros</SelectItem>
+                        <SelectItem value="5">Biceps</SelectItem>
+                        <SelectItem value="6">Triceps</SelectItem>
+                        <SelectItem value="7">Abdominais</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="name"
+                    className="col-span-4"
+                    placeholder="Nome do exercício"
+                  />
+                </div>
+                <div className="grid w-full grid-cols-2 items-center gap-4">
+                  <Input
+                    id="sets"
+                    className="col-span-1"
+                    placeholder="Séries"
+                  />
+                  <Input
+                    id="reps"
+                    className="col-span-1"
+                    placeholder="Repetições"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4 border">
+                  <Textarea
+                    id="description"
+                    className="col-span-4 resize-none"
+                    placeholder="Observações..."
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Save</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </form>
       </div>
     </div>
   )
