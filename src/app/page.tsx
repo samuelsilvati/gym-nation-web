d995b1dbf4eb12c2f59313abbb38ac42fe671887
Samuel Silva <samuel7silva@outlook.com>
2023-08-16 16:55:58 -0300

    fix: home bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between bg-white p-24 dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-between bg-background p-24">
       <Link
         href="/signin"
         className="rounded bg-black px-6 py-2 text-white dark:bg-white dark:text-black"
