0a24d078887c904c57b0c355cb04d1b54df69543
Samuel Silva <samuel7silva@outlook.com>
2023-08-17 18:48:54 -0300

    fix: current date timezone

--- src/components/currentDate.tsx
+++ src/components/currentDate.tsx
@@ -1,9 +1,13 @@
-'use client'
 import React from 'react'
 import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 dayjs.locale('pt-br')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('America/Sao_Paulo')
 
 function CurrentDate() {
   const capitalizeFirstLetter = (str: string) => {
