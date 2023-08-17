4e96691615a9bc8bb6f92189ba3d28fd17b296ab
Samuel Silva <samuel7silva@outlook.com>
2023-08-16 22:00:21 -0300

    fix: change theme provider to providers folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from '@/providers/themeProvider'
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
