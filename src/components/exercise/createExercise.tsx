ceb9dd624ee2dc0e8ff000adda5b3e71e03494d3
Samuel Silva <samuel7silva@outlook.com>
2025-06-01 12:40:28 -0300

    Updates CreateExercise component with new state management and form schema logic
    This commit enhances the CreateExercise component by introducing new state handling with useEffect, adding new state variables, updating the form schema logic, and improving the UI with a switch component for associating exercises from a library. These changes aim to streamline user interaction and provide better control over exercise creation.

--- src/components/exercise/createExercise.tsx
+++ src/components/exercise/createExercise.tsx
@@ -16,7 +16,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { api } from '@/lib/api'
 import { useSession } from 'next-auth/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Loader2 } from 'lucide-react'
 import { useToast } from '../ui/use-toast'
 import {
@@ -27,38 +27,79 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import { Switch } from '../ui/switch'
+import { Label } from '../ui/label'
 
 type DayOfWeekProps = {
   dayOfWeek: string
 }
 
-const createExerciseFormSchema = z.object({
-  name: z.string().nonempty('Campo obrigatório'),
-  description: z.string(),
-  sets: z.string().nonempty('Campo obrigatório'),
-  reps: z.string().nonempty('Campo obrigatório'),
-  muscleGroupId: z
-    .string()
-    .nonempty('Campo obrigatório')
-    .transform((value) => parseInt(value, 10)),
-  dayOfWeekId: z.string().transform((value) => parseInt(value, 10)),
-})
-
 function CreateExercise({ dayOfWeek }: DayOfWeekProps) {
   const [isLoading, setIsLoading] = useState(false)
   const { toast } = useToast()
+  const [associate, setAssociate] = useState(true)
+  const [libraryExercises, setLibraryExercises] = useState<any[]>([])
+  const [selectedMuscleGroup, setSelectedMuscleGroup] = useState<
+    string | undefined
+  >(undefined)
+
+  const createExerciseFormSchema = z.object({
+    name: associate
+      ? z.string().default('')
+      : z.string().nonempty('Campo obrigatório'),
+    description: z.string(),
+    sets: z.string().nonempty('Campo obrigatório'),
+    reps: z.string().nonempty('Campo obrigatório'),
+    muscleGroupId: z
+      .string()
+      .nonempty('Campo obrigatório')
+      .transform((value) => parseInt(value, 10)),
+    dayOfWeekId: z.string().transform((value) => parseInt(value, 10)),
+    exercisesLibId: z
+      .union([z.string().nonempty(), z.undefined()])
+      .transform((value) => (value !== undefined ? value : undefined)),
+  })
+
   type CreateExerciseFormaData = z.infer<typeof createExerciseFormSchema>
 
   const {
     register,
     handleSubmit,
     control,
+    watch,
+    setValue,
     // formState: { errors },
   } = useForm<CreateExerciseFormaData>({
     resolver: zodResolver(createExerciseFormSchema),
   })
 
   const { data: session } = useSession()
+
+  // Buscar exercícios da biblioteca filtrados pelo muscleGroupId
+  useEffect(() => {
+    if (associate && selectedMuscleGroup) {
+      setIsLoading(true)
+      api
+        .get(`/exercisesLib-by-group/${selectedMuscleGroup} `, {
+          headers: {
+            Authorization: `Bearer ${session?.user.token}`,
+          },
+        })
+        .then((res) => {
+          setLibraryExercises(res.data)
+          setIsLoading(false)
+        })
+    }
+  }, [associate, selectedMuscleGroup, session?.user.token])
+
+  const muscleGroupId = watch('muscleGroupId')
+
+  useEffect(() => {
+    setSelectedMuscleGroup(muscleGroupId ? String(muscleGroupId) : undefined)
+    // setValue('exercisesLibId', undefined)
+    // console.log('muscleGroupId mudou:', muscleGroupId)
+    // console.log('exercisesLibId após limpar:', watch('exercisesLibId'))
+  }, [muscleGroupId, setValue, watch])
 
   async function createExercise(data: CreateExerciseFormaData) {
     setIsLoading(true)
@@ -84,7 +125,7 @@ function CreateExercise({ dayOfWeek }: DayOfWeekProps) {
         <Button>Adicionar exercício</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <form onSubmit={handleSubmit(createExercise)}>
+        <form onSubmit={handleSubmit(createExercise, (e) => console.log(e))}>
           <Input
             value={dayOfWeek}
             className="invisible hidden"
@@ -97,15 +138,20 @@ function CreateExercise({ dayOfWeek }: DayOfWeekProps) {
               Adicione o exercício aqui. Clique em adicionar quando terminar.
             </DialogDescription>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-2 items-center gap-4">
-              <Input
-                placeholder="Nome do Exercício"
-                className="col-span-3"
-                {...register('name')}
-                disabled={isLoading}
-              />
-            </div>
+
+          <div className="mb-2 mt-3 flex items-center gap-2">
+            <Switch
+              id="associate-switch"
+              checked={associate}
+              onCheckedChange={setAssociate}
+              disabled={isLoading}
+            />
+            <Label htmlFor="associate-switch">
+              {associate ? 'Associar da biblioteca' : 'Criar novo exercício'}
+            </Label>
+          </div>
+
+          <div className="flex flex-col gap-4 py-4">
             <Controller
               name="muscleGroupId"
               control={control}
@@ -135,6 +181,44 @@ function CreateExercise({ dayOfWeek }: DayOfWeekProps) {
                 </Select>
               )}
             />
+
+            {associate ? (
+              <Controller
+                name="exercisesLibId"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    disabled={isLoading || !selectedMuscleGroup}
+                    value={
+                      field.value === undefined
+                        ? undefined
+                        : String(field.value)
+                    }
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger className=" w-full">
+                      <SelectValue placeholder="Selecione um exercício da biblioteca" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {libraryExercises.map((ex) => (
+                          <SelectItem key={ex.id} value={ex.id}>
+                            {ex.name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            ) : (
+              <Input
+                placeholder="Nome do Exercício"
+                className="col-span-3"
+                {...register('name')}
+                disabled={isLoading}
+              />
+            )}
 
             <div className="grid w-full grid-cols-4 items-center gap-4">
               <Input
