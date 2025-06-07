94195b49f75ff2c703daceba1980ee5320fb48a0
Samuel Silva <samuel7silva@outlook.com>
2025-06-07 13:28:07 -0300

    Adds Toaster component for notifications in workout page
    Integrates the Toaster component into the workout page layout to enhance user experience by providing real-time notifications.
    
    This addition improves the feedback mechanism for users interacting with the workout features.

--- src/app/(protected-routes)/application/workout/[slug]/[id]/page.tsx
+++ src/app/(protected-routes)/application/workout/[slug]/[id]/page.tsx
@@ -1,8 +1,10 @@
 import ShowExercises from '@/components/exercise/showExercises'
+import { Toaster } from '@/components/ui/toaster'
 
 function page({ params }: { params: { slug: string; id: string } }) {
   return (
     <div className="mb-28 flex min-h-screen md:mb-0">
+      <Toaster />
       <ShowExercises slug={params.slug} id={params.id} />
     </div>
   )
