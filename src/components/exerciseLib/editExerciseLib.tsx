2af91a4b249b88827d2a21d1125ec9f0e40f40f8
Samuel Silva <samuel7silva@outlook.com>
2025-06-07 13:28:39 -0300

    Adds EditExerciseLib component for exercise management
    Introduces a new component for editing exercise details, allowing users to update the name, description, and muscle group of exercises.
    
    Implements form validation using Zod and integrates with an API for saving changes. Enhances user experience with loading indicators and toast notifications for feedback.
    
    This component supports client-side interactions in a dialog format, improving the overall exercise management workflow.

---
+++ src/components/exerciseLib/editExerciseLib.tsx
@@ -0,0 +1,172 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '../ui/textarea'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { api } from '@/lib/api'
+import { useSession } from 'next-auth/react'
+import { useState } from 'react'
+import { Loader2 } from 'lucide-react'
+import { useToast } from '../ui/use-toast'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+
+type EditExerciseProps = {
+  id: string
+  name: string
+  description: string
+  muscleGroupId: string | number
+  children?: React.ReactNode
+}
+
+const editExerciseFormSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  muscleGroupId: z
+    .string()
+    .nonempty('Campo obrigatório')
+    .transform((value) => parseInt(value, 10)),
+})
+
+function EditExerciseLib({
+  id,
+  name,
+  description,
+  muscleGroupId,
+  children,
+}: EditExerciseProps) {
+  const { data: session } = useSession()
+  const [isLoading, setIsLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+  const { toast } = useToast()
+  type EditExerciseFormaData = z.infer<typeof editExerciseFormSchema>
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    // formState: { errors },
+  } = useForm<EditExerciseFormaData>({
+    resolver: zodResolver(editExerciseFormSchema),
+    defaultValues: {
+      name,
+      description,
+      muscleGroupId: String(muscleGroupId),
+    } as unknown as EditExerciseFormaData,
+  })
+
+  async function editExercise(data: EditExerciseFormaData) {
+    setIsLoading(true)
+    try {
+      await api.put(`/exerciseLib/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${session?.user.token}`,
+        },
+      })
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: 'Não foi possível salvar exercício',
+        variant: 'destructive',
+      })
+      setIsLoading(false)
+    }
+  }
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <button className="w-full text-start">{children}</button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <form onSubmit={handleSubmit(editExercise, (e) => console.log(e))}>
+          <DialogHeader>
+            <DialogTitle>Editar exercício</DialogTitle>
+            <DialogDescription>
+              Faça alterações no seu exercício aqui. Clique em salvar quando
+              terminar.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-4 py-4">
+            <Controller
+              name="muscleGroupId"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  disabled={isLoading}
+                  value={
+                    field.value === undefined ? undefined : String(field.value)
+                  }
+                  onValueChange={field.onChange}
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Selecione o grupo muscular" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="1">Peito</SelectItem>
+                      <SelectItem value="2">Costas</SelectItem>
+                      <SelectItem value="3">Pernas</SelectItem>
+                      <SelectItem value="4">Ombros</SelectItem>
+                      <SelectItem value="5">Biceps</SelectItem>
+                      <SelectItem value="6">Triceps</SelectItem>
+                      <SelectItem value="7">Abdominais</SelectItem>
+                      <SelectItem value="9">Outros</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+            />
+
+            <Input
+              placeholder="Nome do Exercício"
+              className="col-span-3"
+              {...register('name')}
+              disabled={isLoading}
+            />
+
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Textarea
+                className="col-span-4 resize-none"
+                placeholder="Observações..."
+                {...register('description')}
+                disabled={isLoading}
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                'Salvar'
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default EditExerciseLib
